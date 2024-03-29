e4b7d344582078c5dd886e119380112bb43eee68
MokkaBoss1 <136205220+MokkaBoss1@users.noreply.github.com>
2024-03-29 19:44:31 +0000

    separation of model settings from the main program

--- ComfyUI_MokkaBoss1/nodes/seveninabox.py
+++ ComfyUI_MokkaBoss1/nodes/seveninabox.py
@@ -1,25 +1,8 @@
-# https://github.com/MokkaBoss1/ComfyUI-Mokkaboss1/wiki/Documentation-for-the-ComfyUI-Nodes-in-this-Node-Pack
 import random
 import re
 import folder_paths
 import comfy.sd  # Import the necessary module containing `load_checkpoint_guess_config`
-
-multi_field_list = [
-    [0, 'devlishphotorealism_sdxl15.safetensors', 30, 4.0, 'dpmpp_2m_sde', 'karras'],
-    [1, 'sdxlUnstableDiffusers_v8HEAVENSWRATH.safetensors', 30, 4.0, 'dpmpp_2m_sde', 'karras'],
-    [2, 'lovexlAllInOneMega_v20.safetensors', 30, 4.0, 'dpmpp_2m_sde', 'karras'],
-    [3, 'juggernautXL_v9Rdphoto2Lightning.safetensors', 5, 1.5, 'dpmpp_sde', 'karras'],
-    [4, 'dreamshaperXL_v21TurboDPMSDE.safetensors', 6, 2.0, 'dpmpp_sde', 'karras'],
-    [5, 'devlish LoveXL 050.safetensors', 30, 4.0, 'dpmpp_2m_sde', 'karras'],
-    [6, 'sdxlUnstableDiffusers_v11.safetensors', 30, 4.0, 'dpmpp_2m_sde', 'karras'],
-    [7, 'DEVLISH LIGHTNING.safetensors', 5, 1.5, 'dpmpp_sde', 'karras'],
-    [8, 'sdxlUnstableDiffusers_nihilmania.safetensors', 30, 4.0, 'dpmpp_2m_sde', 'karras'],
-    [9, 'realvisxlV40_v40Bakedvae.safetensors', 30, 4.0, 'dpmpp_2m', 'karras'],
-    [10, 'realvisxlV40_v40LightningBakedvae.safetensors', 5, 1.5, 'dpmpp_sde', 'karras'],
-    [11, 'juggernautXL_v8Rundiffusion.safetensors', 30, 4.0, 'dpmpp_2m', 'karras']
-    
-    # Add more rows as needed
-]
+from ..model_settings.model_data import multi_field_list  # Import multi_field_list
 
 class seveninabox:
 
