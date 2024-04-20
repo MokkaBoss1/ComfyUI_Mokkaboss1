4b3776b7c0fc776f07dad689156faf41ef6ad4dc
MokkaBoss1 <136205220+MokkaBoss1@users.noreply.github.com>
2024-04-20 21:49:44 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,10 +2,7 @@ from ComfyUI_Mokkaboss1.nodes.DoubleClipTextEncode import DoubleClipTextEncode
 from ComfyUI_Mokkaboss1.nodes.HashText import HashText
 from ComfyUI_Mokkaboss1.nodes.IndoorBackgrounds import IndoorBackgrounds
 from ComfyUI_Mokkaboss1.nodes.LandscapeBackgrounds import LandscapeBackgrounds
-from ComfyUI_Mokkaboss1.nodes.NatureColours import NatureColours
 from ComfyUI_Mokkaboss1.nodes.OptimalCrop import OptimalCrop
-from ComfyUI_Mokkaboss1.nodes.seveninabox import seveninabox
-from ComfyUI_Mokkaboss1.nodes.UrbanColours import UrbanColours
 from ComfyUI_Mokkaboss1.nodes.X_In_a_Dress import X_In_a_Dress
 from ComfyUI_Mokkaboss1.nodes.X_In_a_Suit import X_In_a_Suit
 from ComfyUI_Mokkaboss1.nodes.WrapText import WrapText
@@ -34,10 +31,7 @@ NODE_CLASS_MAPPINGS = {
     "HashText": HashText,
     "IndoorBackgrounds": IndoorBackgrounds,   
     "LandscapeBackgrounds": LandscapeBackgrounds,
-    "NatureColours": NatureColours,
     "OptimalCrop": OptimalCrop,
-    "seveninabox": seveninabox,
-    "UrbanColours": UrbanColours,
     "X_In_a_Dress": X_In_a_Dress,
     "X_In_a_Suit": X_In_a_Suit,
     "WrapText": WrapText,
@@ -67,10 +61,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "HashText": "👑 HashText",
     "IndoorBackgrounds": "👑 IndoorBackgrounds",
     "LandscapeBackgrounds": "👑 LandscapeBackgrounds",
-    "NatureColours": "👑 NatureColours(deprecated)",
     "OptimalCrop": "👑 OptimalCrop",
-    "seveninabox": "👑 7inabox(deprecated)",
-    "UrbanColours": "👑 UrbanColours(deprecated)",
     "X_In_a_Dress": "👑 X_In_a_Dress",
     "X_In_a_Suit": "👑 X_In_a_Suit",
     "WrapText": "👑 WrapText",
@@ -94,4 +85,4 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "ImageDimensions": "👑 ImageDimensions",
     "SimplePrompts": "👑 SimplePrompts",
 }
-print ("👑 Mokkaboss1 30 Custom Nodes: Loaded")
+print ("👑 Mokkaboss1 27 Custom Nodes: Loaded")
