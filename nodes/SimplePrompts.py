c7220c644a34f203868fa5ff667382519337a979
MokkaBoss1 <136205220+MokkaBoss1@users.noreply.github.com>
2024-04-20 11:06:46 +0100

    Create Simple Prompts

---
+++ nodes/SimplePrompts.py
@@ -0,0 +1,37 @@
+# https://github.com/MokkaBoss1/ComfyUI-Mokkaboss1/wiki/Documentation-for-the-ComfyUI-Nodes-in-this-Node-Pack
+
+import random
+import re
+
+class SimplePrompts:
+
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {"required": {
+            "pos_prompt": ("STRING", {"default": "Documentation", "multiline": True}),
+            "neg_prompt": ("STRING", {"default": "string", "multiline": True}),
+        }}
+
+    RETURN_TYPES = ("STRING", "STRING")
+    RETURN_NAMES = ("pos_prompt", "neg_prompt",)
+    FUNCTION = "simple_prompts"
+    CATEGORY = "👑 MokkaBoss1/Other"
+
+    def simple_prompts(self, pos_prompt, neg_prompt): 
+        
+        pattern = r"#(.*?)#"
+        clean_text = re.sub(pattern, '', pos_prompt)
+        clean_text2 = clean_text.replace("\n", "")
+        pos_prompt = clean_text2
+        clean_text = re.sub(pattern, '', neg_prompt)
+        clean_text2 = clean_text.replace("\n", "")
+        neg_prompt = clean_text2
+        
+        
+        return (pos_prompt, neg_prompt, )
+
+NODE_CLASS_MAPPINGS = {"SimplePrompts": SimplePrompts}
+NODE_DISPLAY_NAME_MAPPINGS = {"SimplePrompts": "👑 SimplePrompts"}
