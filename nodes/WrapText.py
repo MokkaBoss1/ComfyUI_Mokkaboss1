824f5a37d3b9e3231f481c90626838d12710d459
MokkaBoss1 <136205220+MokkaBoss1@users.noreply.github.com>
2024-05-06 08:42:45 +0100

    Update WrapText.py

--- nodes/WrapText.py
+++ nodes/WrapText.py
@@ -14,31 +14,18 @@ class WrapText:
             "width": ("INT", {"default": 50, "min": 1, "max": 300, "step": 1}),
         }}
 
-    RETURN_TYPES = ("STRING","STRING","STRING","STRING","STRING","STRING","STRING",)
-    RETURN_NAMES = ("Line1","Line2","Line3","Line4","Line5","Line6","Line7",)
-    FUNCTION = "test"
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("text",)
+    FUNCTION = "wrap_text"
     CATEGORY = "👑 MokkaBoss1/Text"
 
-    def test(self, text, width): 
-
-        print ("Wrap Text Node")
-        print (text)
-        print (width)
-
-        line_one = ""
-        line_two = ""
-        line_three = ""
-        line_four = ""
-        line_five = ""
-        line_six = ""
-        line_seven = ""
-
+    def wrap_text(self, text, width): 
         wrapped_lines = []
         words = text.split()
         current_line = ''
             
         for word in words:
-            if len(current_line) + len(word) <= width:
+            if len(current_line) + len(word) + 1 <= width:  # +1 for space
                 current_line += word + ' '
             else:
                 wrapped_lines.append(current_line.strip())
@@ -46,42 +33,12 @@ class WrapText:
             
         # Append the remaining text
         if current_line:
-             wrapped_lines.append(current_line.strip())
+            wrapped_lines.append(current_line.strip())
 
-        # Assign wrapped lines if they exist
-        if len(wrapped_lines) >= 1:
-            line_one = wrapped_lines[0]
-        if len(wrapped_lines) >= 2:
-            line_two = wrapped_lines[1]
-        if len(wrapped_lines) >= 3:
-            line_three = wrapped_lines[2]
-        if len(wrapped_lines) >= 4:
-            line_four = wrapped_lines[3]
-        if len(wrapped_lines) >= 5:
-            line_five = wrapped_lines[4]
-        if len(wrapped_lines) >= 6:
-            line_six = wrapped_lines[5]
-        if len(wrapped_lines) >= 7:
-            line_seven = wrapped_lines[6]
+        # Dynamically create the output with line breaks
+        text = "\n".join(wrapped_lines)
 
-        # Print the lines
-        print("start")
-        print(line_one)
-        print(line_two)
-        print(line_three)
-        print(line_four)
-        print(line_five)
-        print(line_six)
-        print(line_seven)
-        print("End")
-
-        return (line_one, line_two, line_three, line_four, line_five, line_six, line_seven)
+        return (text, )
 
 NODE_CLASS_MAPPINGS = {"WrapText": WrapText}
 NODE_DISPLAY_NAME_MAPPINGS = {"WrapText": "👑 WrapText"}
-
-
-
-# 👑
-
-
