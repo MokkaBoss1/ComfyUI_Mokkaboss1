5105f16b36f577bb7e5fcea3db3883f4f6bf8b7e
MokkaBoss1 <136205220+MokkaBoss1@users.noreply.github.com>
2024-05-07 18:27:22 +0100

    Create SaveWithMetaData2.py

---
+++ nodes/SaveWithMetaData2.py
@@ -0,0 +1,170 @@
+# https://github.com/MokkaBoss1/ComfyUI-Mokkaboss1/wiki/Documentation-for-the-ComfyUI-Nodes-in-this-Node-Pack
+# this node is based on the "Save w/ meta data"node from giriss \ comfy-image-saver (https://github.com/giriss/comfy-image-saver)
+# I noticed the original node was taking a long time to process so after debugging I found that reading the modelhash was the culprit. This step is removed in this version of the node.
+from datetime import datetime
+import os
+from datetime import datetime
+import json
+import piexif
+import piexif.helper
+from PIL import Image, ExifTags
+from PIL.PngImagePlugin import PngInfo
+import numpy as np
+import folder_paths
+import comfy.sd
+from nodes import MAX_RESOLUTION
+
+
+def parse_name(ckpt_name):
+    path = ckpt_name
+    filename = path.split("/")[-1]
+    filename = filename.split(".")[:-1]
+    filename = ".".join(filename)
+    return filename
+
+
+def handle_whitespace(string: str):
+    return string.strip().replace("\n", " ").replace("\r", " ").replace("\t", " ")
+
+
+def get_timestamp(time_format):
+    now = datetime.now()
+    try:
+        timestamp = now.strftime(time_format)
+    except:
+        timestamp = now.strftime("%Y-%m-%d-%H%M%S")
+
+    return timestamp
+
+
+def make_pathname(filename, seed, modelname, counter, time_format):
+    filename = filename.replace("%date", get_timestamp("%Y-%m-%d"))
+    filename = filename.replace("%time", get_timestamp(time_format))
+    filename = filename.replace("%model", modelname)
+    filename = filename.replace("%seed", str(seed))
+    filename = filename.replace("%counter", str(counter))
+    return filename
+
+
+def make_filename(filename, seed, modelname, counter, time_format):
+    filename = make_pathname(filename, seed, modelname, counter, time_format)
+
+    return get_timestamp(time_format) if filename == "" else filename
+
+
+
+
+def date_string(format):        
+        
+    now = datetime.now()
+    return now.strftime(format)
+
+class SaveWithMetaData:
+
+    def __init__(self):
+        self.output_dir = folder_paths.output_directory
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "images": ("IMAGE", ),
+#                "filename": ("STRING", {"default": f'%time_%seed', "multiline": False}),
+#                "path": ("STRING", {"default": '', "multiline": False}),
+                "extension": (['png', 'jpeg', 'webp'],),
+                "steps": ("INT", {"default": 20, "min": 1, "max": 10000}),
+                "cfg": ("FLOAT", {"default": 8.0, "min": 0.0, "max": 100.0}),
+                "modelname_string": ("STRING", {"default": '', "multiline": False}),
+                "sampler_name_string": ("STRING", {"default": '', "multiline": False}),
+                "scheduler_string": ("STRING", {"default": '', "multiline": False}),
+            },
+            "optional": {
+                "positive": ("STRING", {"default": 'unknown', "multiline": True}),
+                "negative": ("STRING", {"default": 'unknown', "multiline": True}),
+                "seed_value": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
+                "width": ("INT", {"default": 512, "min": 1, "max": MAX_RESOLUTION, "step": 8}),
+                "height": ("INT", {"default": 512, "min": 1, "max": MAX_RESOLUTION, "step": 8}),
+                "lossless_webp": ("BOOLEAN", {"default": True}),
+                "quality_jpeg_or_webp": ("INT", {"default": 100, "min": 1, "max": 100}),
+#                "counter": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff }),
+#                "time_format": ("STRING", {"default": "%Y-%m-%d-%H%M%S", "multiline": False}),
+            },
+            "hidden": {
+                "prompt": "PROMPT",
+                "extra_pnginfo": "EXTRA_PNGINFO"
+            },
+        }
+
+    RETURN_TYPES = ()
+    FUNCTION = "save_files"
+
+    OUTPUT_NODE = True
+
+    CATEGORY = "👑 MokkaBoss1/Other"
+
+    def save_files(self, images, seed_value, steps, cfg, sampler_name_string, scheduler_string, positive, negative, modelname_string, quality_jpeg_or_webp,
+                   lossless_webp, width, height, extension, prompt=None, extra_pnginfo=None):
+        
+        counter = 0
+        time_format = ""
+        format1 = "(%Y-%m-%d)(%H-%M-%S)"
+        format2 = "(%Y-%m-%d)"
+        
+        filename = date_string(format1)
+        path = str(date_string(format2) + "/" + date_string(format2) + "_" + extension)
+        
+        filename = make_filename(filename, seed_value, modelname_string, counter, time_format)
+        path = make_pathname(path, seed_value, modelname_string, counter, time_format)
+        ckpt_path = folder_paths.get_full_path("checkpoints", modelname_string)
+        
+        comment = f"Positive: {handle_whitespace(positive)}\nNegative prompt: {handle_whitespace(negative)}\nSteps: {steps}, Sampler: {sampler_name_string},Scheduler: {scheduler_string}, CFG Scale: {cfg}, Seed: {seed_value}, Size: {width}x{height}, Model: {modelname_string}, Version: ComfyUI"
+        output_path = os.path.join(self.output_dir, path)
+
+        if output_path.strip() != '':
+            if not os.path.exists(output_path.strip()):
+                print(f'The path `{output_path.strip()}` specified doesn\'t exist! Creating directory.')
+                os.makedirs(output_path, exist_ok=True)    
+
+        filenames = self.save_images(images, output_path, filename, comment, extension, quality_jpeg_or_webp, lossless_webp, prompt, extra_pnginfo)
+
+        subfolder = os.path.normpath(path)
+        return {"ui": {"images": map(lambda filename: {"filename": filename, "subfolder": subfolder if subfolder != '.' else '', "type": 'output'}, filenames)}}
+
+    def save_images(self, images, output_path, filename_prefix, comment, extension, quality_jpeg_or_webp, lossless_webp, prompt=None, extra_pnginfo=None) -> list[str]:
+        img_count = 1
+        paths = list()
+        for image in images:
+            i = 255. * image.cpu().numpy()
+            img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+            if images.size()[0] > 1:
+                filename_prefix += "_{:02d}".format(img_count)
+
+            if extension == 'png':
+                metadata = PngInfo()
+                metadata.add_text("parameters", comment)
+
+                if prompt is not None:
+                    metadata.add_text("prompt", json.dumps(prompt))
+                if extra_pnginfo is not None:
+                    for x in extra_pnginfo:
+                        metadata.add_text(x, json.dumps(extra_pnginfo[x]))
+
+                filename = f"{filename_prefix}.png"
+                img.save(os.path.join(output_path, filename), pnginfo=metadata, optimize=True)
+            else:
+                filename = f"{filename_prefix}.{extension}"
+                file = os.path.join(output_path, filename)
+                img.save(file, optimize=True, quality=quality_jpeg_or_webp, lossless=lossless_webp)
+                exif_bytes = piexif.dump({
+                    "Exif": {
+                        piexif.ExifIFD.UserComment: piexif.helper.UserComment.dump(comment, encoding="unicode")
+                    },
+                })
+                piexif.insert(exif_bytes, file)
+
+            paths.append(filename)
+            img_count += 1
+        return paths
+
+NODE_CLASS_MAPPINGS = {"SaveWithMetaData": SaveWithMetaData}
+NODE_DISPLAY_NAME_MAPPINGS = {"SaveWithMetaData": "👑 SaveWithMetaData"}
