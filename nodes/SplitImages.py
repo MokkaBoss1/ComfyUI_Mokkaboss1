d7a23bc1963fd579f4ff1630abb8f3b7125a3edc
MokkaBoss1 <136205220+MokkaBoss1@users.noreply.github.com>
2024-06-03 17:48:28 +0100

    Update SplitImages.py

--- nodes/SplitImages.py
+++ nodes/SplitImages.py
@@ -40,8 +40,8 @@ class SplitImages:
         
         images = []
         
-        for i in range(row):
-            for j in range(column):
+        for i in range(rows):
+            for j in range(columns):
                 left = j * tile_width
                 upper = i * tile_height
                 right = left + tile_width
